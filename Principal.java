84950c3de914699a903aca1c89250d0ec9cb9fc3
DevHgL <hleomelo1@icloud.com>
2023-12-07 09:52:47 -0300

    Finalizado

---
+++ Principal.java
@@ -0,0 +1,27 @@
+public class Principal
+{
+    public static void main(String[] args) 
+    {
+        EstoqueProdutos estoque = new EstoqueProdutos();
+        estoque.adicionaProduto(new ProdutoEstoque("monitor", 1_450, 20));
+        estoque.adicionaProduto(new ProdutoEstoque("telefone", 3_000, 300));
+        estoque.adicionaProduto(new ProdutoEstoque("teclado", 400, 50));
+        estoque.adicionaProduto(new ProdutoEstoque("mouse", 350, 50));
+        
+        CarrinhoCompra carrinho = new CarrinhoCompra(estoque);
+        carrinho.adicionaItem("monitor", 2);
+        carrinho.adicionaItem("telefone", 5);
+        carrinho.adicionaItem("teclado", 2);             
+
+        System.out.printf("A soma dos produtos: R$ %.2f%n", carrinho.calculaTotal());
+
+        carrinho.finalizaCompra();
+
+        System.out.printf("A soma dos produtos apos a compra: R$ %.2f%n", carrinho.calculaTotal());
+
+        for (int i = 0; i < estoque.getQuantidadeProdutos();i++)
+        {
+            System.out.println("Quantidade atual de " + estoque.getProduto(i).getNome()+ " no estoque: " + estoque.getProduto(i).getQuantidade());
+        }
+    }
+}
