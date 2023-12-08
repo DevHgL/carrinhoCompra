f2cf0953b67c9275d7d3eefa542d5a6223795c24
Hugo Leo <135613370+DevHgL@users.noreply.github.com>
2023-12-08 12:22:37 -0300

    Update PrecoProdutos.java

--- PrecoProdutos.java
+++ PrecoProdutos.java
@@ -4,6 +4,7 @@ public class PrecoProdutos
     private float valor;
     private int quantidade;
 
+    // Construtor para inicializar os atributos da classe com valores fornecidos
     public PrecoProdutos(String nome, float valor, int quantidade)
     {
         this.nome = nome;
@@ -11,25 +12,31 @@ public class PrecoProdutos
         this.quantidade = quantidade;
     }
 
+    // Método para obter o nome do produto
     public String getNome()
     {
         return nome;
     }
 
+    // Método para obter o valor do produto
     public float getValor()
     {
         return valor;
     }
 
+    // Método para obter a quantidade do produto
     public int getQuantidade()
     {
         return quantidade;
     }
 
+    // Método para definir a quantidade do produto
     public void setQuantidade(int quantidade)
     {
         this.quantidade = quantidade;
     }
+
+    // Método para definir o valor do produto
     public void setValor(int valor)
     {
         this.valor = valor;
